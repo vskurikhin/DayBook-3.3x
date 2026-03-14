c7b4945710dae68f4b7c89afc623c67481b7cce4
Victor N. Skurikhin <VSkurikhin@Skala-R.Ru>
2026-03-14 23:27:53 +0300

    Close #xx: "In Auth Module ..."
    ...

--- auth/cmd/server/wire/di.go
+++ auth/cmd/server/wire/di.go
@@ -11,7 +11,10 @@ import (
 	"github.com/vskurikhin/DayBook-3.3x/auth/v2/internal/server/db"
 	"github.com/vskurikhin/DayBook-3.3x/auth/v2/internal/server/env"
 	"github.com/vskurikhin/DayBook-3.3x/auth/v2/internal/server/handler"
+	"github.com/vskurikhin/DayBook-3.3x/auth/v2/internal/server/repository/user_name"
+	"github.com/vskurikhin/DayBook-3.3x/auth/v2/internal/server/repository/user_view"
 	"github.com/vskurikhin/DayBook-3.3x/auth/v2/internal/server/resources"
+	"github.com/vskurikhin/DayBook-3.3x/auth/v2/internal/server/services"
 )
 
 var (
@@ -33,9 +36,22 @@ var (
 		resources.NewV1,
 		resources.NewV2,
 	)
+	serviceSet = wire.NewSet(
+		wire.Bind(new(services.BaseService), new(*services.AuthBaseService)),
+		services.NewAuthBaseService,
+	)
+	repositorySet = wire.NewSet(
+		wire.Bind(new(user_name.Repo), new(*user_name.Queries)),
+		wire.Bind(new(user_name.DBTX), new(*db.PgxPool)),
+		wire.Bind(new(user_view.Repo), new(*user_view.Queries)),
+		wire.Bind(new(user_view.DBTX), new(*db.PgxPool)),
+		db.GetDB,
+		user_name.New,
+		user_view.New,
+	)
 )
 
-func InitializeServer(cfg config.Config, environments env.Environments, dbp db.DB) (*server.AuthServer, error) {
-	wire.Build(serverSet, handlerSet, resourceSet)
+func InitializeServer(cfg config.Config, environments env.Environments) (*server.AuthServer, error) {
+	wire.Build(serverSet, handlerSet, resourceSet, serviceSet, repositorySet)
 	return &server.AuthServer{}, nil
 }
--- auth/internal/server/resources/resource_v2.go
+++ auth/internal/server/resources/resource_v2.go
@@ -5,16 +5,22 @@ import (
 	"net/http"
 
 	"github.com/vskurikhin/DayBook-3.3x/auth/v2/internal/server/config"
+	"github.com/vskurikhin/DayBook-3.3x/auth/v2/internal/server/dto"
+	"github.com/vskurikhin/DayBook-3.3x/auth/v2/internal/server/services"
 )
 
+//go:generate mockgen -destination=resource_v2_mock_test.go -package=resources github.com/vskurikhin/DayBook-3.3x/auth/v2/internal/server/resources ResourceV2
 type ResourceV2 interface {
+	Auth(w http.ResponseWriter, r *http.Request) error
 	Ok(w http.ResponseWriter, r *http.Request) error
+	Register(w http.ResponseWriter, r *http.Request) error
 }
 
 var _ ResourceV2 = (*V2)(nil)
 
 type V2 struct {
-	cfg config.Config
+	cfg     config.Config
+	service services.BaseService
 }
 
 // Ok route
@@ -40,8 +46,66 @@ func (v V2) Ok(w http.ResponseWriter, _ *http.Request) error {
 	})
 }
 
+// Auth route
+// @Summary Auth Краткое содержание
+// @Description Auth - Описание (v2)
+// @ID ResourceV2-auth
+// @Tags    auth
+// @Accept  json
+// @Produce json
+// @Param   request body dto.Login true "Request of auth"
+// @Success 200 {object} APIResponse{data=dto.Token} "успешно"
+// @Failure 500 {object} APIResponse{error=string,success=bool} "ошибка сервера "success": false"
+// @Failure 504
+// @Router /v2/auth [post]
+func (v V2) Auth(w http.ResponseWriter, r *http.Request) error {
+	decoder := json.NewDecoder(r.Body)
+	var login dto.Login
+	errDecode := decoder.Decode(&login)
+	if errDecode != nil {
+		return errDecode
+	}
+	token, err := v.service.Auth(r.Context(), services.UserFromDto(login))
+	if err != nil {
+		return err
+	}
+	return json.NewEncoder(w).Encode(APIResponse{
+		Success: true,
+		Data:    token.ToDto(),
+	})
+}
+
+// Register route
+// @Summary Register Краткое содержание
+// @Description Register - Описание (v2)
+// @ID ResourceV2-register
+// @Tags    register
+// @Accept  json
+// @Produce json
+// @Param   request body dto.Login true "Request of register"
+// @Success 200 {object} APIResponse{data=dto.CreatedUser} "успешно"
+// @Failure 500 {object} APIResponse{error=string,success=bool} "ошибка сервера "success": false"
+// @Failure 504
+// @Router /v2/register [post]
+func (v V2) Register(w http.ResponseWriter, r *http.Request) error {
+	decoder := json.NewDecoder(r.Body)
+	var login dto.Login
+	errDecode := decoder.Decode(&login)
+	if errDecode != nil {
+		return errDecode
+	}
+	user, err := v.service.Register(r.Context(), services.UserFromDto(login))
+	if err != nil {
+		return err
+	}
+	return json.NewEncoder(w).Encode(APIResponse{
+		Success: true,
+		Data:    user.ToDto(),
+	})
+}
+
 // NewV2 creates and returns a new V2 resource instance that implements
 // the ResourceV2 interface.
-func NewV2(cfg config.Config) *V2 {
-	return &V2{cfg: cfg}
+func NewV2(cfg config.Config, service services.BaseService) *V2 {
+	return &V2{cfg: cfg, service: service}
 }
