1dbc153206620fd866136eeaaec02b97a165604f
Victor <vskurikhin@lnhost.net>
2026-03-16 10:05:25 +0300

    Close #45 (#46)
    Co-authored-by: Victor N. Skurikhin <VSkurikhin@GMail.Com>

--- auth/cmd/server/cmd/migrations/20260313194929_uuid_postgresql.sql
+++
@@ -1,7 +0,0 @@
--- +goose Up
-SELECT 'up SQL query';
-CREATE EXTENSION IF NOT EXISTS "uuid-ossp" SCHEMA pg_catalog version "1.1";
-
--- +goose Down
-SELECT 'down SQL query';
-DROP EXTENSION IF EXISTS "uuid-ossp";
