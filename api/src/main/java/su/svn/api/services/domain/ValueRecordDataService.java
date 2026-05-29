15a3cb2ee3e4a257de6e857bc675860dc70cd4af
Victor <vskurikhin@lnhost.net>
2026-05-29 21:04:14 +0300

    Fixed #135 (#136)
    Co-authored-by: Victor N. Skurikhin <VSkurikhin@GMail.Com>

--- api/src/main/java/su/svn/api/services/domain/ValueRecordDataService.java
+++ api/src/main/java/su/svn/api/services/domain/ValueRecordDataService.java
@@ -1,5 +1,5 @@
 /*
- * This file was last modified at 2026.05.22 18:49 by Victor N. Skurikhin.
+ * This file was last modified at 2026.05.29 20:51 by Victor N. Skurikhin.
  * This is free and unencumbered software released into the public domain.
  * For more information, please refer to <http://unlicense.org>
  * ValueRecordDataService.java
@@ -14,9 +14,7 @@ import jakarta.inject.Inject;
 import su.svn.api.models.dto.NewValueRecord;
 import su.svn.api.models.dto.ResourceValueRecord;
 import su.svn.api.models.dto.UpdateValueRecord;
-import su.svn.api.repository.PostRecordRepository;
 import su.svn.api.repository.ValueRecordRepository;
-import su.svn.api.services.mappers.ValueRecordMapper;
 
 import java.util.UUID;
 
@@ -24,30 +22,26 @@ import java.util.UUID;
 public class ValueRecordDataService {
 
     @Inject
-    ValueRecordRepository recordRepository;
+    ValueRecordRepository repository;
 
     @Inject
-    ValueRecordMapper mapper;
-
-    @Inject
-    PostRecordRepository postRecordRepository;
+    ValueRecordSyncTrigger trigger;
 
     public Uni<Void> delete(UUID id) {
-        return Uni.combine().all().unis(
-                recordRepository.delete(id),
-                postRecordRepository.disable(id)
-        ).withUni(l -> Uni.createFrom().voidItem());
+        return repository.delete(id)
+                .onItem()
+                .invoke(unused -> trigger.accept(id));
     }
 
-    public Uni<ResourceValueRecord> post(NewValueRecord newValueRecord) {
-        return recordRepository.post(newValueRecord);
+    public Uni<ResourceValueRecord> post(NewValueRecord record) {
+        return repository.post(record)
+                .onItem()
+                .invoke(trigger);
     }
 
-    public Uni<ResourceValueRecord> put(UpdateValueRecord updateValueRecord) {
-        return recordRepository.put(updateValueRecord)
-                .flatMap(resourceJsonRecord ->
-                        postRecordRepository.update(mapper.toEntity(updateValueRecord))
-                                .map(postRecord -> mapper.toResource(postRecord))
-                );
+    public Uni<ResourceValueRecord> put(UpdateValueRecord record) {
+        return repository.put(record)
+                .onItem()
+                .invoke(trigger);
     }
 }
